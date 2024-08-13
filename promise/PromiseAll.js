f23555ad9d465094e7f2f0715063084e7f955676
likai <2806699104@qq.com>
2024-08-13 16:22:32 +0800

    测试

--- promise/PromiseAll.js
+++ promise/PromiseAll.js
@@ -1,3 +1,10 @@
+/*
+ * @FilePath: \html-test\promise\PromiseAll.js
+ * @Author: likai 2806699104@qq.com
+ * @Version: 0.0.1
+ * @LastEditors: likai 2806699104@qq.com
+ * @Description:  
+ */
 
 var p1 = new Promise((resoleve, reject) => {
     setTimeout(() => {
@@ -17,20 +24,20 @@ var p1 = new Promise((resoleve, reject) => {
   });
    
   //第一情况
-//    var promiseArr = [p1, p2, p3];
-//    console.time("promiseArr");
-    // Promise.all(promiseArr)
-    //   .then((res) => {
-    //     console.log("res", res); //res [ 'p1--3000', 'p2--1000', 'p3--5000' ]
-    //     console.timeEnd("promiseArr"); // promiseArr: 5.020s
-    //   })
-    //   .catch((err) => console.log(err));
+  //  var promiseArr = [p1, p2, p3];
+  //  console.time("promiseArr");
+  //   Promise.all(promiseArr)
+  //     .then((res) => {
+  //       console.log("res", res); //res [ 'p1--3000', 'p2--1000', 'p3--5000' ]
+  //       console.timeEnd("promiseArr"); // promiseArr: 5.020s
+  //     })
+  //     .catch((err) => console.log(err));
 
       //另外情况
 var p4 = new Promise((resoleve, reject) => {
     setTimeout(() => {
       reject("p4--2000");
-    }, 2000);
+    }, 1000);
   });
    
   var promiseArr = [p1, p2, p3, p4];
